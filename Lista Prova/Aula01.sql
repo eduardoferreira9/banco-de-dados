d70ec0dfed108f1dbddbf75f36d3f861b2ac6b1a
eduardoferreira9 <ef02830@gmail.com>
2025-05-13 13:36:31 -0300

    add: Lista de Exercicios/Aula01.sql

---
+++ Lista Prova/Aula01.sql
@@ -0,0 +1,13 @@
+-- Considerando a tabela a seguir:
+CREATE TABLE numero(
+    c1 SERIAL,
+    c2 INTEGER,
+    c3 NUMERIC(4,2),
+    c4 NUMERIC(3,1)
+);
+
+-- a. Qual nome da tabela criada? R: numero
+
+-- b. Quantos campos temos dentro da tabela? R: 4
+
+-- c. Qual o menor e o maior valor que pode ser armazenado dentro do campo c3? R: o maior será 99.99 e o menor -99.99
